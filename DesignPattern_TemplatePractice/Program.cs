1b0b7de5918cf78837d07fa662e8dcdb1ff2d4a6
noneiori <noneiori@gmail.com>
2021-07-30 22:21:06 +0800

    加入專案檔案。

---
+++ DesignPattern_TemplatePractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_TemplatePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter8Tests.Test1();
+            Chapter8Tests.Test2();
+        }
+    }
+}
